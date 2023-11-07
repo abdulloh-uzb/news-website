3aa135dcc8ed5b99c4823f2adfcf0b6697b679c9
abdulloh-uzb <uabdulloh11@gmail.com>
2023-11-07 05:51:35 +0500

    finished about page

---
+++ resources/views/frontend/about.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.frontend')
+
+@section('content')
+<div class="container-fluid mt-5 pt-3">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-8">
+                <div class="section-title mb-0">
+                    <h4 class="m-0 text-uppercase font-weight-bold">Biz haqimizda</h4>
+                </div>
+                <div class="bg-white border border-top-0 p-4 mb-3">
+                    <div>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit, corporis deserunt? Corporis vitae suscipit fugiat architecto, doloremque natus. Harum, suscipit similique totam maiores nam doloremque vel odio necessitatibus. Incidunt harum hic quos quas fuga error maxime natus quod, sit magni asperiores, veniam eveniet expedita suscipit aliquam, impedit labore nostrum nihil!</p>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
